e36fc9b254cb8fd2309c13e28ff6792f409e1a59
Stanislau <xmix25@gmail.com>
2023-05-06 14:43:20 +0300

    stdin/stdout

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log('Hello, Node.js');
+const { stdin, stdout, stderr } = process;
+stdin.on('data', (data) => stdout.write(data));
+process.on('exit', (code) => {
+  if (code === 0) {
+    stdout.write('OK');
+  } else {
+    stderr.write(`Something get wrong! Programm end with code ${code}`);
+  }
+});
+process.exit(55);
