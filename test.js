cbea00d3bcecb8e7ff10b06c09ab33a57218d09b
Stanislau <xmix25@gmail.com>
2023-05-06 15:27:11 +0300

    input/output task 2

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-const { stdin, stdout } = process;
-stdout.write('Enter your name \n');
+const { stdin, stdout, exit } = process;
+
+stdout.write('Enter your name ');
 stdin.on('data', (name) => {
-  stdout.write(`Hello, ${name}`);
-  process.exit();
+  const nameStr = name.toString().split('').reverse().join('');
+  stdout.write(`\nYour reverse name is: ${nameStr}`);
+  exit();
 });
 process.on('exit', () => {
-  stdout.write('Goodbye');
+  stdout.write('\nGoodluck!');
 });
+
+// const { stdin, stdout } = process;
+
+// stdout.write('Как тебя зовут?\n');
+// stdin.on('data', (data) => {
+//   const name = data.toString();
+//   const reverseName = name.split('').reverse().join('');
+//   stdout.write(`\nТвоё имя наоборот ${reverseName}`);
+//   process.exit();
+// });
