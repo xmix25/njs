40bf56c21d7fc4dadb588cd1095e3d678862be20
Stanislau <xmix25@gmail.com>
2023-05-06 17:33:52 +0300

    command line arguments task

--- test.js
+++ test.js
@@ -1,30 +1,43 @@
-const { stdout } = process;
+// const { stdout, stdin } = process;
+// stdout.write('Enter 2 numbers\n> ');
+// stdin.on('data', (data) => {
+//   const numberArr = data.toString().split(' ');
+//   const firstNum = parseInt(numberArr[0], 10);
+//   const secondNum = parseInt(numberArr[1], 10);
+//   if (process.argv.includes('-m')) {
+//     stdout.write(`${firstNum} * ${secondNum} = ${firstNum * secondNum}`);
+//   } else if (process.argv.includes('-s')) {
+//     stdout.write(`${firstNum} + ${secondNum} = ${firstNum + secondNum}`);
+//   }
+//   process.exit();
+// });
 
-// const flagIndex = process.argv.indexOf('-m');
-// if (flagIndex !== -1) {
-//   const message = process.argv[flagIndex + 1];
-//   stdout.write(message);
-// }
-
-// function getValue(flag) {
-//   const flagIndex = process.argv.indexOf(flag);
-//   return flagIndex !== -1 ? process.argv[flagIndex + 1] : null;
-// }
-// stdout.write(getValue('-m'));
-
-// const protoObj = {
-//   sayHi() {
-//     stdout.write('Hi!');
-//   },
-// };
-
-// const obj = {};
-// obj.__proto__ = protoObj;
-// obj.sayHi();
-
-const productionMode = process.env.PRODUCTION === 'true';
-if (productionMode) {
-  stdout.write('Application is running in production mode');
-} else {
-  stdout.write('Application is running in development mode');
+const { stdout, stdin, exit } = process;
+const flag = process.argv[2];
+const allowedFlags = ['-m', '-s'];
+if (!allowedFlags.includes(flag)) {
+  stdout.write('Попробуйте ещё раз запустить файл с флагом -s или -m');
+  exit();
 }
+stdout.write('Введите, пожалуйста, два числа\n');
+stdin.on('data', (data) => {
+  const numString = data.toString();
+  const numStringsArray = numString.split(' ');
+  const hasIncorrectLength = numStringsArray.length !== 2;
+  const hasIncorrectValues = numStringsArray.some((numStr) =>
+    Number.isNaN(+numStr)
+  );
+  if (hasIncorrectLength || hasIncorrectValues) {
+    stdout.write('Нужно ввести 2 числа, разделенных пробелом');
+    exit();
+  }
+  const [firstNum, secondNum] = numStringsArray.map((numStr) => +numStr);
+  if (flag === '-s') {
+    const sum = firstNum + secondNum;
+    stdout.write(`${firstNum} + ${secondNum} = ${sum}`);
+  } else {
+    const mult = firstNum * secondNum;
+    stdout.write(`${firstNum} * ${secondNum} = ${mult}`);
+  }
+  exit();
+});
