b17711280180c8fa3ce04352d10d50ca3f4ff4d4
Stanislau <xmix25@gmail.com>
2023-05-06 17:54:20 +0300

    filesystem task

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
-const { stdout } = process;
+const { stdout, exit } = process;
 
-stdout.write(__dirname);
-stdout.write('\n');
-stdout.write(__filename);
+const flag = process.argv[2];
+const availableFlas = ['-d', '-f'];
+process.on('exit', (code) => {
+  if (code === 1) {
+    stdout.write('start with "-d" or "-f" flag');
+  } else {
+    stdout.write('\nBye');
+  }
+});
+if (!flag || !availableFlas.includes(flag)) {
+  exit(1);
+}
+const value = flag === '-d' ? __dirname : __filename;
+stdout.write(value);
+exit();
