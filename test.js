5031e4c24893661b81bb3cd3f25020fa898ee120
Stanislau <xmix25@gmail.com>
2023-05-07 17:31:27 +0300

    fs module task

--- test.js
+++ test.js
@@ -1,44 +1,82 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'notes'), { recursive: true }, (err, p) => {
-  if (err) throw err;
-  console.log('Папка создана');
-  console.log(p);
-});
+const [command, title, content] = process.argv.slice(2);
 
-fs.writeFile(
-  path.join(__dirname, 'notes', 'mynotes.txt'),
-  'Hello world!',
-  (err) => {
-    if (err) throw err;
-    console.log('Файл был создан');
-  }
-);
+function init() {
+  fs.writeFile('notes.json', '[]', (error) => {
+    if (error) return console.error(error.message);
+    return undefined;
+  });
+}
 
-fs.appendFile(
-  path.join(__dirname, 'notes', 'mynotes.txt'),
-  ' From append file',
-  (err) => {
-    if (err) throw err;
-    console.log('Файл был изменен');
-  }
-);
+function create(title, content) {
+  fs.access(path.resolve(__dirname, 'notes.json'), fs.constants.F_OK, (err) => {
+    if (err) init();
 
-fs.readFile(
-  path.join(__dirname, 'notes', 'mynotes.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
+    fs.readFile('notes.json', (error, data) => {
+      if (error) return console.error(error.message);
+      const notes = JSON.parse(data);
+      notes.push({ title, content });
+      const json = JSON.stringify(notes);
 
-fs.rename(
-  path.join(__dirname, 'notes', 'mynotes.txt'),
-  path.join(__dirname, 'notes', 'notes.txt'),
-  (err) => {
-    if (err) throw err;
-    console.log('Файл переименован');
-  }
-);
+      fs.writeFile('notes.json', json, (error) => {
+        if (error) return console.error(error.message);
+        console.log('Заметка создана');
+      });
+    });
+  });
+}
+
+function list() {
+  fs.readFile(path.resolve(__dirname, 'notes.json'), (err, data) => {
+    if (err) return console.log(err);
+    const notes = JSON.parse(data);
+    notes.forEach((note, index) => {
+      console.log(`${index + 1} ${note.title}`);
+    });
+  });
+}
+
+function view() {
+  fs.readFile(path.resolve(__dirname, 'notes.json'), (err, data) => {
+    if (err) return console.error(err.message);
+    const notes = JSON.parse(data);
+    const note = notes.find((note) => note.title === title);
+    if (!note) {
+      console.log('Заметка не найдена');
+    } else {
+      console.log(note.content);
+    }
+    return;
+  });
+}
+function remove() {
+  fs.readFile('notes.json', (err, data) => {
+    if (err) return console.error(err.message);
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.title !== title);
+    const json = JSON.stringify(notes);
+    fs.writeFile(path.resolve(__dirname, 'notes.json'), json, (err) => {
+      if (err) return console.error(err.message);
+      console.log('заметка удалена');
+    });
+    return undefined;
+  });
+}
+switch (command) {
+  case 'list':
+    list();
+    break;
+  case 'view':
+    view();
+    break;
+  case 'create':
+    create(title, content);
+    break;
+  case 'remove':
+    remove();
+    break;
+  default:
+    console.log('Unknown command');
+}
