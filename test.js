9b224956fb279b64399fa40feeae4ef73b083cef
Stanislau <xmix25@gmail.com>
2023-05-07 17:51:50 +0300

    os module

--- test.js
+++ test.js
@@ -1,82 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const os = require('os');
 
-const [command, title, content] = process.argv.slice(2);
+// Платформа
+console.log(os.platform());
 
-function init() {
-  fs.writeFile('notes.json', '[]', (error) => {
-    if (error) return console.error(error.message);
-    return undefined;
-  });
-}
+// Архитектура
+console.log(os.arch());
 
-function create(title, content) {
-  fs.access(path.resolve(__dirname, 'notes.json'), fs.constants.F_OK, (err) => {
-    if (err) init();
+// Информация о CPU
+console.log(os.cpus());
 
-    fs.readFile('notes.json', (error, data) => {
-      if (error) return console.error(error.message);
-      const notes = JSON.parse(data);
-      notes.push({ title, content });
-      const json = JSON.stringify(notes);
+// Общий объём памяти
+console.log(os.totalmem());
 
-      fs.writeFile('notes.json', json, (error) => {
-        if (error) return console.error(error.message);
-        console.log('Заметка создана');
-      });
-    });
-  });
-}
+// Объём свободной памяти
+console.log(os.freemem());
 
-function list() {
-  fs.readFile(path.resolve(__dirname, 'notes.json'), (err, data) => {
-    if (err) return console.log(err);
-    const notes = JSON.parse(data);
-    notes.forEach((note, index) => {
-      console.log(`${index + 1} ${note.title}`);
-    });
-  });
-}
+// Корневая директория
+console.log(os.homedir());
 
-function view() {
-  fs.readFile(path.resolve(__dirname, 'notes.json'), (err, data) => {
-    if (err) return console.error(err.message);
-    const notes = JSON.parse(data);
-    const note = notes.find((note) => note.title === title);
-    if (!note) {
-      console.log('Заметка не найдена');
-    } else {
-      console.log(note.content);
-    }
-    return;
-  });
-}
-function remove() {
-  fs.readFile('notes.json', (err, data) => {
-    if (err) return console.error(err.message);
-    let notes = JSON.parse(data);
-    notes = notes.filter((note) => note.title !== title);
-    const json = JSON.stringify(notes);
-    fs.writeFile(path.resolve(__dirname, 'notes.json'), json, (err) => {
-      if (err) return console.error(err.message);
-      console.log('заметка удалена');
-    });
-    return undefined;
-  });
-}
-switch (command) {
-  case 'list':
-    list();
-    break;
-  case 'view':
-    view();
-    break;
-  case 'create':
-    create(title, content);
-    break;
-  case 'remove':
-    remove();
-    break;
-  default:
-    console.log('Unknown command');
-}
+// Время работы системы
+console.log(os.uptime());
+
+// Символ окончания строки в данной системе
+console.log(os.EOL);
